5697fcd01c2c39aedf44bbec76fe2081abb2d24f
benjamin antonioli <benantonioli@outlook.fr>
2022-10-28 10:40:54 +0200

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,8 @@
-const FIZZ_NUMBER = process.env.FIZZ_NUMBER;
+const FIZZ_NUMBER = +process.env.FIZZ_NUMBER;
 
 function fizzbuzz(FIZZ_NUMBER) {
-    
-  if (FIZZ_NUMBER < 0 || FIZZ_NUMBER > 20) {
+
+  if (FIZZ_NUMBER < 1 || FIZZ_NUMBER > 20) {
     return console.log("I accept numbers from 1 to 20");
 
   } else if (FIZZ_NUMBER % 3 === 0 && FIZZ_NUMBER % 5 === 0) {
